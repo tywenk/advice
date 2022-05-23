051d63230f2fdcdf372d34df0a5d2236c7a2472a
Tywen Kelly <tywen.kelly@gmail.com>
2022-05-23 16:57:15 -0700

    adds layout

---
+++ components/navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Navbar = () => {
+	return <div className='bg-red-100 w-full'>Navbar</div>
+}
+
+export default Navbar
--- pages/list.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-const list = () => {
-	return <div>list</div>
-}
-
-export default list
