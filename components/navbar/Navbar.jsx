e2d4c198a92fd7dae778828e6df7dcb2e51d6efe
Tywen Kelly <tywen.kelly@gmail.com>
2022-05-23 17:57:47 -0700

    adds comp diagram and list route

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -1,7 +1,19 @@
-import React from "react"
+import Link from "next/link"
 
 const Navbar = () => {
-	return <div className='bg-red-100 w-full'>Navbar</div>
+	return (
+		<div className='bg-red-100 w-full flex justify-around'>
+			<Link href='/'>
+				<a>Home</a>
+			</Link>
+			<Link href='/list'>
+				<a>List</a>
+			</Link>
+			<Link href='/grid'>
+				<a>Grid</a>
+			</Link>
+		</div>
+	)
 }
 
 export default Navbar
