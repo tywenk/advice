911510745668fa1a4f088e042a5a048fa8474875
Tywen Kelly <tywen.kelly@gmail.com>
2022-05-25 22:20:16 -0700

    getadvice from api

--- pages/api/all.js
+++ pages/api/all.js
@@ -1,11 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import fs from "fs/promises"
-import path from "path"
+import getAdviceData from "../../utils/getAdviceData"
 
 export default async function handler(req, res) {
-	const filePath = path.join(process.cwd(), "data", "advice.json")
-	const jsonData = await fs.readFile(filePath)
-	const advice = await JSON.parse(jsonData)
+	const advice = await getAdviceData()
 
 	res.status(200).json(advice)
 }
